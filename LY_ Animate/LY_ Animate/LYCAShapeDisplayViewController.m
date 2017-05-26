a4f3e076ce584d2554b37b4a6b0497b4a08064d0
张泽 <zhangzeStrong@126.com>
2017-05-26 15:16:12 +0800

    wave starting

--- LY_ Animate/LY_ Animate/LYCAShapeDisplayViewController.m
+++ LY_ Animate/LY_ Animate/LYCAShapeDisplayViewController.m
@@ -12,6 +12,14 @@
 
 @property(nonatomic, strong) CADisplayLink *displayLink;
 
+//波浪动画参数属性
+@property(nonatomic, strong) UIBezierPath *wavePath;
+@property(nonatomic, assign) int waveWidth; //
+@property(nonatomic, assign) int maxAmplitude; //曲线的波峰值
+@property(nonatomic, assign) int frequency; //频率
+@property(nonatomic, assign) int phase;
+@property(nonatomic, assign) CGRect bounds;
+
 @end
 
 @implementation LYCAShapeDisplayViewController
@@ -21,10 +29,20 @@
     self.view.backgroundColor = [UIColor whiteColor];
     // Do any additional setup after loading the view.
     
-//    _displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(displayVIew)];
-//    [_displayLink addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSRunLoopCommonModes];
+
     
-    [self displayVIew];
+    [self drawPeople];
+    
+    _wavePath = [[UIBezierPath alloc] init];
+    _waveWidth = 0;
+    _maxAmplitude = 50;
+    _frequency = 1;
+    _phase = 0;
+    _bounds = CGRectMake(0, 180+100+150+50, 320, 200);
+    
+    _displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(drawWave)];
+    [_displayLink addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSRunLoopCommonModes];
+
 }
 
 - (void)didReceiveMemoryWarning {
@@ -32,13 +50,41 @@
     // Dispose of any resources that can be recreated.
 }
 
-//使用贝塞尔曲线绘制火柴人
-- (void)drawPeople
+
+- (void)displayVIew
 {
     
 }
 
-- (void)displayVIew
+- (void)updateWave:(CADisplayLink *)displayLink
+{
+    self.phase += 8;//逐渐累加初相
+    self.wavePath.CGPath = [self drawWave].CGPath;
+}
+
+//绘制波浪线
+-(UIBezierPath *)drawWave
+{
+    
+    
+    CGFloat endX = 0;
+    for (CGFloat x = 0; x < self.waveWidth + 1; x += 1) {
+        endX=x;
+        CGFloat y = self.maxAmplitude * sinf(360.0 / _waveWidth * (x  * M_PI / 180) * self.frequency + self.phase * M_PI/ 180) + self.maxAmplitude;
+        if (x == 0) {
+            [_wavePath moveToPoint:CGPointMake(x, y)];
+        } else {
+            [_wavePath addLineToPoint:CGPointMake(x, y)];
+        }
+    }
+    CGFloat endY = CGRectGetHeight(self.bounds) + 10;
+    [_wavePath addLineToPoint:CGPointMake(endX, endY)];
+    [_wavePath addLineToPoint:CGPointMake(0, endY)];
+    return _wavePath;
+}
+
+//使用贝塞尔曲线绘制火柴人
+- (void)drawPeople
 {
     //使用贝塞尔曲线保存绘制路径
     UIBezierPath *bezierPath = [[UIBezierPath alloc] init];
@@ -48,8 +94,8 @@
     [bezierPath addArcWithCenter:CGPointMake(150, 180) radius:100.0f startAngle:0 endAngle:2*M_PI clockwise:YES];
    
         //添加竖直线（身躯）
-    [bezierPath moveToPoint:CGPointMake(150, 180+100)];
-    [bezierPath addLineToPoint:CGPointMake(150, 180+100+150)];
+    [bezierPath moveToPoint:CGPointMake(150, 180+100)];  //移动到要开始绘制的地方
+    [bezierPath addLineToPoint:CGPointMake(150, 180+100+150)]; //绘制一条线条到指定位置
     
         //添加横线（手臂）
     [bezierPath moveToPoint:CGPointMake(50, 180+100)];
